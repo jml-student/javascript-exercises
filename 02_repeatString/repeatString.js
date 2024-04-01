571ecd60f3cd579406e6c33225cacf381866849e
jml-student <student741@proton.me>
2024-04-01 15:03:18 +0200

    Commit 02_repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0) return "ERROR";
+    let repeatedWord = "";
+    for (i = 0; i < number; i++) {
+        repeatedWord += string;
+    }
+    return repeatedWord;
 };
 
 // Do not edit below this line
