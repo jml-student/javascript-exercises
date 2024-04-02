3844b7de5ed3c7cd27fb1ee7720c2b5bb73d5aed
jml-student <student741@proton.me>
2024-04-02 13:20:59 +0200

    Commit 05_sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    if (firstInt < 0 || secondInt < 0) return "ERROR";
 
+    if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt)) return "ERROR";
+    
+    if (firstInt > secondInt) {
+        const temp = secondInt;
+        secondInt = firstInt;
+        firstInt = temp;
+    }
+    
+    let sum = 0;
+
+    for (let i = firstInt; i <= secondInt; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
