ac408f8dfe03190b8ab460453af7ad3ea94273e4
jml-student <student741@proton.me>
2024-04-17 21:13:13 +0200

    Commit 10_fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }
 
+    const fibonacci = [0, 1];
+
+    for (i=0; i<number; i++) {
+        fibonacci.push(fibonacci[fibonacci.length - 2] + fibonacci[fibonacci.length - 1]);
+    }
+    
+    return fibonacci[fibonacci.length - 2];
 };
 
 // Do not edit below this line
