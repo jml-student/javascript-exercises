4b7e9f63c538d12a8a6cb79a9ec9f5953223e361
jml-student <student741@proton.me>
2024-04-17 14:23:42 +0200

    Commit 09_palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
 
-};
+
+    const reversed = cleanedString
+    .split("")
+    .reverse()
+    .join("");
+
+    return cleanedString === reversed;
+};  
 
 // Do not edit below this line
 module.exports = palindromes;
