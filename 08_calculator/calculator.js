61269322fc375f4d426d1f92d29558024ff9c05d
jml-student <student741@proton.me>
2024-04-17 13:58:11 +0200

    Commit 08_calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, num) => sum * num);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  for (i=a; i>1; i--) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
